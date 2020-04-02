e1782f8b76ebdecd3812315be7fbf315d17623d1
ANAMPEDIA <namdevel@users.noreply.github.com>
2020-04-02 09:49:46 +0700

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,7 +1,8 @@
 import requests
 import re
+import sys
 
-text_to_convert = 'namdevel'
+text_to_convert = sys.argv[1]
 
 url = 'https://www.sljfaq.org/cgi/kanjiabc.cgi'
 myobj = {'abcin': text_to_convert, 'oneormulti': 0}
