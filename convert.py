fd5bb87a7b00058efdf7fe11f12385b60e452dcf
ANAMPEDIA <namdevel@users.noreply.github.com>
2020-04-02 09:53:43 +0700

    Update convert.py

--- convert.py
+++ convert.py
@@ -5,7 +5,7 @@ import sys
 text_to_convert = sys.argv[1]
 
 url = 'https://www.sljfaq.org/cgi/kanjiabc.cgi'
-myobj = {'abcin': text_to_convert, 'oneormulti': 0}
+myobj = {'abcin': text_to_convert, 'oneormulti': '1'}
 
 x = requests.post(url, data = myobj)
 kanji = re.findall(r'<td class="kanji-out">(.*?)</td>', x.text)[0]
